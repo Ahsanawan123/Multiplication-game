daa2f4b2b13db76d626050de43d1ca2a240764a0
Ahsanawan123 <98630461+Ahsanawan123@users.noreply.github.com>
2023-11-30 20:06:36 -0600

    Final Update WinCond.asm
    - Everything works fine besides diagonal function
    - function now jumps through so you only need to call checkHOrizontalWin, rest commence automatically
    - diagonally half checks the board half doesn't but besides that everything is good
    - uploaded on 11/30/23 at 8:06pm

--- WinCond.asm
+++ WinCond.asm
@@ -3,10 +3,7 @@ PlayerWinMssg: .asciiz "Player Wins!\n"
 CompWinMssg: .asciiz "Computer Wins!\n"
 DrawMssg: .asciiz "It's a draw!\n"
 
-
 .text
-
-# Part a
 # Function to check for horizontal win condition
 .globl checkHorizontalWin
 checkHorizontalWin:
@@ -19,7 +16,8 @@ checkHorizontalWin:
     # $t5 - player number (1 or 2, depending on whose turn it is)
     # $t6 - index calculation
     # $t7 - constant value 4 for comparison
-# Function: Check for a horizontal win
+    # $t8 - used to load the arrray element to add to address offset
+    # $t9 - temporary register do display if user/computer wins
 
     # Load the turn into $t5 to be used in win-checking functions
     lw $t5, turn
@@ -84,6 +82,18 @@ win_horizontal:
        
 # Function: Check for a vertical win
 .globl checkVerticalWin
+# Registers:
+    # $t0 - base address of status
+    # $t1 - Coloumn counter
+    # $t2 - row counter
+    # $t3 - temporary value for comparison
+    # $t4 - consecutive count
+    # $t5 - player number (1 or 2, depending on whose turn it is)
+    # $t6 - index calculation
+    # $t7 - constant value 4 for comparison
+    # $t8 - used to load the arrray element to add to address offset
+    # $t9 - temporary register do display if user/computer wins
+    
 checkVerticalWin:
     lw $t5, turn
 
@@ -121,7 +131,6 @@ vertical_row_loop:
 
     continue_vertical:
         beq $t4, $t7, win_vertical # Check if four in a row is reached
-        # addi $t2, $t2, 1   # Move to the next row
         j next_row_vertical
 
     reset_consecutive_vertical:
@@ -149,6 +158,18 @@ win_vertical:
     
 # Function: Check for an ascending diagonal win
 .globl checkDiagonalWinAscending
+# Registers:
+    # $t0 - base address of status
+    # $t1 - row counter
+    # $t2 - column counter
+    # $t3 - temporary value for comparison
+    # $t4 - consecutive count
+    # $t5 - player number (1 or 2, depending on whose turn it is)
+    # $t6 - index calculation
+    # $t7 - constant value 4 for comparison
+    # $t8 - used to load the arrray element to add to address offset
+    # $t9 - temporary register do display if user/computer wins/ Index change for ascending diagonal
+    
 checkDiagonalWinAscending:
     la $t0, status   # Load base address of the status array
     lw $t5, turn     # Load the current player's number
@@ -161,90 +182,93 @@ checkDiagonalWinAscending:
         li $t3, 0         # Counter for checking four in a diagonal
 
         check_consecutive_ascending:
-            bge $t3, $t7, next_diagonal_ascending # Check four positions
+            bge $t3, 4, next_diagonal_ascending # Check four positions
 
-            mul $t6, $t3, 5  # Index increases by 5 for each step in ascending diagonal
-    	    sub $t6, $t1, $t6 # Subtract since we're moving up and to the right
+            # Calculate index for ascending diagonal up till halfway (coloumn)
+            li $t9, 6       # Index change for ascending diagonal
+            mul $s5, $t3, $t9
+            sub $t6, $t1, $s5
+            add $t6, $t6, $t3
+            
             sll $t6, $t6, 2  # Convert to word offset
             add $t6, $t0, $t6 # Address in the status
 
             lw $t8, 0($t6)   # Load the element
-            bne $t8, $t5, reset_consecutive_descending # If not matching, check next
+            bne $t8, $t5, reset_consecutive_ascending # If not matching, check next
             addi $t4, $t4, 1 # Increment consecutive count
 
             addi $t3, $t3, 1 # Move to the next element in the diagonal
             j check_consecutive_ascending
+            
+        check_consecutive_ascending_left:
+            bge $t3, 4, next_diagonal_ascending # Check four positions
+
+            # Calculate index for ascending diagonal up till halfway (coloumn)
+            li $t9, 6       # Index change for ascending diagonal
+            mul $s5, $t3, $t9
+            add $t6, $t1, $s5
+            add $t6, $t6, $t3
+            
+            sll $t6, $t6, 2  # Convert to word offset
+            add $t6, $t0, $t6 # Address in the status
+
+            lw $t8, 0($t6)   # Load the element
+            bne $t8, $t5, reset_consecutive_ascending # If not matching, check next
+            addi $t4, $t4, 1 # Increment consecutive count
+
+            addi $t3, $t3, 1 # Move to the next element in the diagonal
+            j check_consecutive_ascending_left
 
         next_diagonal_ascending:
-            beq $t4, $t7, win_diagonal_ascending # Check if four in a row is reached
-            addi $t1, $t1, 1   # Move to the next starting index to the right
-    	    blt $t1, 33, ascending_diagonal_loop # If index less than 33, continue checking
-
+            beq $t4, $t7, win_diagonal_ascending # Check if four in a row is reached	
+        
+            # Adjust for the next diagonal index
+            
+            beq $t1, 32, change33
+            beq $t1, 26, change27
+            beq $t1, 20, change21
+            
+    	    beq $t1, 15, change35
+    	    beq $t1, 9, change29
+    	    beq $t1, 3, change23
+    	    addi $t1, $t1, 1
+    	    
+       reset_consecutive_ascending:
+    	    li $t3, 0          # Reset consecutive count
+    	    j next_diagonal_ascending
+    	    
         check_next:
-    	addi $t1, $t1, 6   # Move up to start checking next diagonal
-    	blt $t1, 36, ascending_diagonal_loop # If index less than 36, continue checking
-
-    	li $v0, 0          # No win found
-    	j checkDiagonalWinDescending    # Proceed to check for a draw
+    	    addi $t1, $t1, 1   # Move to the next starting index to the right
+            j ascending_diagonal_loop
+            
+    	    li $v0, 0          # No win found
+    	    j checkForDraw    # Proceed to check for a draw
+            
+    	change33:
+    	    li $t1, 24
+    	    j check_consecutive_ascending
+    	change27:
+    	    li $t1, 18
+    	    j check_consecutive_ascending
+    	change21:
+    	    li $t1, 12
+    	    j check_consecutive_ascending_left
+    	change35:
+    	    li $t1, 6
+    	    j check_consecutive_ascending_left
+    	change29: 
+    	    li $t1, 0
+    	    j check_consecutive_ascending_left
+    	change23:
+    	    j checkForDraw
+    	    
 
 win_diagonal_ascending:
      lw $t9, turn             # Win found
      beq $t9, 1, playerWin
      beq $t9, 2, compWin
      j exitProgram
-
-# Function to check for a descending diagonal win
-.globl checkDiagonalWinDescending
-checkDiagonalWinDescending:
-    
-    # Function: Check for a descending diagonal win
-    la $t0, status   # Load base address of the status array
-    lw $t5, turn        # Assuming it's the player's turn (use 2 for the computer)
-    li $t7, 4        # Count for a win condition
-    
-    # Starting indices for potential descending diagonals on a 6x6 board
-    # (The actual indices should be calculated based on your game board's layout)
-    li $t1, 0        # Top-left corner index for the first diagonal
-    
-    descending_diagonal_loop:
-    	li $t4, 0        # Reset consecutive count
-    	li $t3, 0         # Inner loop counter for checking four in a diagonal
-    
-    check_consecutive_descending:
-        bge $t3, $t7, next_diagonal_descending  # Check four positions
-        
-        # Calculate index in status for descending diagonal
-        mul $t6, $t3, 7  # Index increases by 7 for each step in descending diagonal
-        add $t6, $t6, $t1
-        sll $t6, $t6, 2  # Convert to word offset
-        add $t6, $t0, $t6 # Address in the status
-        
-        lw $t8, 0($t6)   # Load the element
-        bne $t8, $t5, reset_consecutive_descending # If not matching, check next
-        addi $t4, $t4, 1 # Increment consecutive count
-        
-        addi $t3, $t3, 1 # Move to the next element in the diagonal
-        j check_consecutive_descending
-        
-    next_diagonal_descending:
-	beq $t4, $t7, win_diagonal_descending # Check if four in a row is reached
-
-	# Decide whether to move right or left for the next check
-    	addi $t1, $t1, 1
-    	blt $t1, 18, descending_diagonal_loop # If index less than 18, check the next diagonal
-    
-        li $v0, 0          # No win found
-        j checkForDraw     # Jump to check for draw before returning
-
-reset_consecutive_descending:
-    li $t4, 0          # Reset consecutive count
-    j next_diagonal_descending
-
-win_diagonal_descending:
-    lw $t9, turn       # Win found
-    beq $t9, 1, playerWin
-    beq $t9, 2, compWin
-    j exitProgram      # Exit the program after a win
+     
 playerWin:
     li $v0, 4                  # syscall for print string
     la $a0, PlayerWinMssg
@@ -260,11 +284,21 @@ compWin:
 exitProgram:
     li $v0, 10                 # syscall for exit
     syscall
-  
     
 # Part b
 # Validates move and checks for draw
 .globl checkForDraw
+# Registers:
+    # $t0 - base address of status
+    # $t1 - row counter
+    # $t2 - column counter
+    # $t3 - temporary value for comparison
+    # $t4 - consecutive count
+    # $t5 - player number (1 or 2, depending on whose turn it is)
+    # $t6 - index calculation
+    # $t7 - constant value 4 for comparison
+    # $t8 - used to load the arrray element to add to address offset
+    # $t9 - temporary register do display if user/computer wins/ Index change for ascending diagonal
 checkForDraw:
     la $t0, slider        # Load base address of the slider array
     la $t1, status        # Load base address of the status array
@@ -323,14 +357,11 @@ check_draw:
 
 valid_move_found:
     # If a valid move is found, simply return
-    j jumpToMain #jr ra
+    j jumpToMain 
     
 # Part c
 .globl jumpToMain
 jumpToMain:
-    #j main         # Jump to the main label
+  
     jr $ra
     nop            # Following the jump with a nop is a good practice to handle the delay slot
-
-
-    
